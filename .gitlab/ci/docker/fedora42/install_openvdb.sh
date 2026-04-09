ae14807f8b459af0cd639032c8c101626fb22844
Cory Quammen <cory.quammen@kitware.com>
2026-04-09 10:20:49 -0400

    fedora42: backport patch from openvdb 13.0.0 needed for nanovdb use

--- .gitlab/ci/docker/fedora42/install_openvdb.sh
+++ .gitlab/ci/docker/fedora42/install_openvdb.sh
@@ -21,6 +21,55 @@ sha256sum --check openvdb.sha256sum
 
 tar -C "$openvdb_src" --strip-components=1 -xf "$openvdb_tarball"
 
+# backport patch from openvdb 13.0.0 to fix build on clang
+# source: https://github.com/AcademySoftwareFoundation/openvdb/commit/930c3acb8e0c7c2f1373f3a70dc197f5d04dfe74
+cd "$openvdb_src/"
+echo "diff --git a/nanovdb/nanovdb/util/GridBuilder.h b/nanovdb/nanovdb/util/GridBuilder.h
+index 30385661d0..428215ba65 100644
+--- a/nanovdb/nanovdb/util/GridBuilder.h
++++ b/nanovdb/nanovdb/util/GridBuilder.h
+@@ -1158,7 +1158,7 @@ struct LeafNode
+         ValueIterator& operator=(const ValueIterator&) = default;
+         ValueType operator*() const { NANOVDB_ASSERT(*this); return mParent->mValues[mPos];}
+         Coord getCoord() const { NANOVDB_ASSERT(*this); return mParent->offsetToGlobalCoord(mPos);}
+-        bool isActive() const { NANOVDB_ASSERT(*this); return mParent->isActive(mPos);}
++        bool isActive() const { NANOVDB_ASSERT(*this); return mParent->mValueMask.isOn(mPos);}
+         operator bool() const {return mPos < SIZE;}
+         ValueIterator& operator++() {++mPos; return *this;}
+         ValueIterator operator++(int) {
+diff --git a/openvdb/openvdb/tree/NodeManager.h b/openvdb/openvdb/tree/NodeManager.h
+index 27a3f82012..1023c00748 100644
+--- a/openvdb/openvdb/tree/NodeManager.h
++++ b/openvdb/openvdb/tree/NodeManager.h
+@@ -327,7 +327,7 @@ class NodeList
+         void operator()(const NodeRange& range) const
+         {
+             for (typename NodeRange::Iterator it = range.begin(); it; ++it) {
+-                OpT::template eval(mNodeOp, it);
++                OpT::eval(mNodeOp, it);
+             }
+         }
+         const NodeOp mNodeOp;
+@@ -347,7 +347,7 @@ class NodeList
+         void operator()(const NodeRange& range) const
+         {
+             for (typename NodeRange::Iterator it = range.begin(); it; ++it) {
+-                OpT::template eval(mNodeOp, it);
++                OpT::eval(mNodeOp, it);
+             }
+         }
+         const NodeOp& mNodeOp;
+@@ -372,7 +372,7 @@ class NodeList
+         void operator()(const NodeRange& range)
+         {
+             for (typename NodeRange::Iterator it = range.begin(); it; ++it) {
+-                OpT::template eval(*mNodeOp, it);
++                OpT::eval(*mNodeOp, it);
+             }
+         }
+         void join(const NodeReducer& other)
+" | patch -p1
+
 cd "$openvdb_build"
 
 cmake -GNinja "$openvdb_src/" \
