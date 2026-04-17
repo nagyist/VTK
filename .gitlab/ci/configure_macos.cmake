a670c08d437ba6d299222514aad501d9c8233789
Ben Boeckel <ben.boeckel@kitware.com>
2026-04-17 14:42:30 -0400

    ci: download a Python on macOS
    Previously, we relied on the Python provided by XCode, but it is no
    longer guaranteed to be at least VTK's minimum supported Python.
    Instead, download a suitable Python.

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -32,8 +32,17 @@ set(VTK_ENABLE_CATALYST OFF CACHE BOOL "") # catalyst
 # vtk issue: https://gitlab.kitware.com/vtk/vtk/-/issues/19903
 set(VTK_USE_PCH OFF CACHE BOOL "")
 
-# Add rpath entries for Xcode frameworks.
-set(CMAKE_BUILD_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
-set(CMAKE_INSTALL_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
+set(rpaths
+  # Add rpath entries for Xcode frameworks.
+  "$ENV{DEVELOPER_DIR}/Library/Frameworks")
+
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "python")
+  list(APPEND rpaths
+    # Add rpath entry for the CI-downloaded Python.
+    "$ENV{GIT_CLONE_PATH}/.gitlab/python/Python.framework")
+endif ()
+
+set(CMAKE_BUILD_RPATH "${rpaths}" CACHE STRING "")
+set(CMAKE_INSTALL_RPATH "${rpaths}" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
